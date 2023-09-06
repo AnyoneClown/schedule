c5f5ccaf7c593683986db637c9a9a30549d532da
Denis Dymerlii <dum.denya15@gmail.com>
2023-09-07 00:30:20 +0300

    ready

---
+++ main.py
@@ -0,0 +1,77 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+import time, os
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+
+class SeleniumActions:
+    def __init__(self):
+        self.options = webdriver.ChromeOptions()
+        self.driver = webdriver.Chrome(options=self.options)
+
+    def open_website(self, url):
+        self.driver.get(url)
+        WebDriverWait(self.driver, 10).until(
+            lambda x: x.execute_script("return document.readyState === 'complete'")
+        )
+
+    def values(self, group):
+        self.driver.find_element(By.ID, "group").send_keys(group)
+        self.driver.find_element(By.XPATH, '//*[@id="wrap"]/div/div/div/div[2]/form/div[3]/div[3]/button').click()
+
+    def get_dates(self, soup):
+        divs = soup.find_all("div", class_="col-md-6")
+        dates = []
+        for div in divs:
+            headers = div.find_all("h4")
+            dates.extend(headers)
+        return dates
+
+    def get_tables(self, soup):
+        divs = soup.find_all("div", class_="col-md-6")
+        tables = []
+        for div in divs:
+            table = div.find("table")
+            if table:
+                tables.append(table)
+        return tables
+
+    def parse(self):
+        soup = bs(self.driver.page_source, "lxml")
+        dates = self.get_dates(soup)
+        tables = self.get_tables(soup)
+        return dates, tables
+
+    def visualize(self, dates, tables):
+
+        tables = pd.read_html(str(tables))
+
+        output_folder = "C:\\Programming\\python\\Pet Project\\schedule"
+
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        output_file = os.path.join(output_folder, "розклад.txt")
+
+        with open(output_file, 'w', encoding="utf-8") as file:
+            for i, table in enumerate(tables):
+                if i < len(dates):
+                    current_date = dates[i].text
+                    file.write(f"{current_date}\n\n")
+
+                if table is not None:  # Check if table is not None before using it
+                    file.write(f"Таблиця {i + 1}:\n")
+                    file.write(table.to_string(index=False) + "\n\n")
+
+
+    def close(self):
+        self.driver.close()
+        self.driver.quit()
+
+selenium_actions = SeleniumActions()
+selenium_actions.open_website("https://dekanat.kubg.edu.ua/cgi-bin/timetable.cgi?n=700")
+selenium_actions.values("ІНб22240д")
+dates, tables = selenium_actions.parse()
+selenium_actions.visualize(dates, tables)
+selenium_actions.close()
