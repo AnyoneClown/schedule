5ffddaf7d58d988b9b25252fe091eb97b6c74fb0
Denis Dymerlii <dum.denya15@gmail.com>
2023-09-09 20:03:39 +0300

    updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-import time, os
+import os
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 from tabulate import tabulate
